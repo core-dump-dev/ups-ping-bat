a5450559ddc3d89a9de7a6c79369e5e6bb5e1cbc
core_dmp <seresha.maltsev.2003@gmail.com>
2026-09-10 23:12:37 +0800

    не только локалхост

--- run.bat
+++ run.bat
@@ -1,5 +1,21 @@
 @echo off
 title UPS Web Monitor
+echo ==================================================
+echo  UPS Web Monitor
+echo ==================================================
+echo.
+echo  NOTE: If you want to access the web UI from other
+echo  devices on your network, run this ONCE as admin
+echo  in PowerShell:
+echo.
+echo    netsh http add urlacl url=http://+:9921/ sddl=D:(A;;GX;;;S-1-1-0)
+echo.
+echo  And open the firewall port:
+echo.
+echo    New-NetFirewallRule -DisplayName "UPS Web UI" -Direction Inbound -Protocol TCP -LocalPort 9921 -Action Allow
+echo.
+echo ==================================================
+echo.
 echo Starting UPS Web Monitor...
 echo Web UI: http://localhost:9921/
 echo Press Ctrl+C to stop.
